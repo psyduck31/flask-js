b6b44d07a0721e3daa552b0912a62de3db86b90b
psyduck31 <47318921+psyduck31@users.noreply.github.com>
2020-03-01 21:38:41 +0300

    Update sort.js

--- website/static/sort.js
+++ website/static/sort.js
@@ -12,16 +12,26 @@ function rpad(string, len) {
 
 
 function printStudents(students, parameter, value) {
+	//Stundets - массив студентов, parameter - параметр для сортировки (оценки или название группы), value - значение для сортировки (название группы или ср. балл)
 	console.log(rpad("Имя", 15), rpad("Фамилия", 15), rpad("Группа", 8), rpad("Оценки", 20))
+	//Начинаем цикл для сортировки
 	for (var i = 0; i < students.length; i++) {
+		//obj - это часть из списка одного студента, которая должна сортироваться (оценки или название группы), parameter - параметры для сортировки (marks или group)
 		let obj = students[i][parameter];
+		//Если obj имеет тип данных "object", в данном случае имеется ввиду если obj это массив
 		if (typeof obj == "object") {
 			let marks = 0;
+			//Так как obj это массив, то мы складываем все значения
 			for (var j = 0; j < obj.length; j++) {
 				marks += obj[j];
 			}
+			//Получаем ср. оценку
 			marks / obj.length >= parseFloat(value) ? console.log(rpad(students[i].name, 15), rpad(students[i].surname, 15), rpad(students[i].group, 8), rpad(students[i].marks, 20)) : null;
 		}
+		//Эта 'функция' выполняется только если obj содержит value, то есть если parameter это название группы, а в значении value
+		//записано 'БСТ', то он выведет этот объект только если в названии группы студента есть слово 'БСТ'
+		//Сам синтаксис этой записи такой
+		// условие ? Эта часть выполняется если условие true : эта часть выполняется если условие false;
 		obj.includes(value) ? console.log(rpad(students[i].name, 15), rpad(students[i].surname, 15), rpad(students[i].group, 8), rpad(students[i].marks, 20)) : null;
 		}
 	alert("Результат выведен в консоль!")
